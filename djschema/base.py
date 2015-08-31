86c08667fa38c14845bb9693e60ab1cb93f46dbd
Di Wu <dw323@cornell.edu>
2015-08-30 17:03:31 -0700

    schema aware introspection

--- djschema/base.py
+++ djschema/base.py
@@ -1,18 +1,22 @@
 from django.db.backends.postgresql_psycopg2.base import DatabaseWrapper as Base
 
+from .introspection import DatabaseIntrospection
+
 
 class DatabaseWrapper(Base):
 
     def __init__(self, *args, **kwargs):
         super(DatabaseWrapper, self).__init__(*args, **kwargs)
 
-        schema = self.settings_dict.get('SCHEMA')
-        if schema:
+        self.schema = self.settings_dict.get('SCHEMA', 'public')
+        if self.schema == 'public':
+            self._init_sql = None
+        else:
             self._init_sql = '''
                 CREATE SCHEMA IF NOT EXISTS "{schema}";SET SCHEMA '{schema}';
-                '''.format(schema=schema)
-        else:
-            self._init_sql = None
+                '''.format(schema=self.schema)
+
+        self.introspection = DatabaseIntrospection(self)
 
     def init_connection_state(self):
         super(DatabaseWrapper, self).init_connection_state()
